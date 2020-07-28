c329ef25c2c72ae3931a4b30f982862f2ad2efc5
Victor Avila <avilavictor1570@gmail.com>
2020-07-28 04:05:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,11 +30,19 @@ class Song
   end
   
   def self.find_by_name(string_name_of_the_song)
-    self.all.detect
+    self.all.detect {|i| i.name == string_name_of_the_song}
   end
   
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+  def self.find_or_create_by_name(find_this_song)
+    did_i_find_it == self.all.detect{|x| x.name == find_this_song}
+    if did_i_find_it == nil
+      s = self.new
+      s.name = find_this_song
+      s.save
+      s
+    else
+      did_i_find_it
+    end
   end
   
   def self.alphabetical()
